c3df9a7cfeb2593bbaf27c5199e3425904334d62
Craig Iverson <civerson1@gmail.com>
2018-10-14 09:45:21 -0700

    war additions to app and pom

--- src/main/java/io/openshift/booster/BoosterApplication.java
+++ src/main/java/io/openshift/booster/BoosterApplication.java
@@ -18,12 +18,19 @@ package io.openshift.booster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import  org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BoosterApplication {
+public class BoosterApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BoosterApplication.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoosterApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BoosterApplication.class, args);
+	}
 
 }
